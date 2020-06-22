5d5767949b2f5bd624d8128c7f41464570a767c9
root <chimerakang@gmail.com>
2020-06-22 14:51:28 +0800

    Fix docker files

--- docker.sh
+++ docker.sh
@@ -1,2 +1,5 @@
 mkdir -p /home/data/simple_file_server/files
-docker run -d --name simple_file_server --restart=always -p 2077:2077 -v /home/data/simple_file_server/files/:/go/src/github.com/chimera/simple_file_server/downloads chimerakang/simeple_file_server
+docker build -t chimerakang/simple_file_server .
+docker stop simple_file_server
+docker rm simple_file_server
+docker run -d --name simple_file_server --restart=always -p 2077:2077 -v /home/data/simple_file_server/files/:/go/src/github.com/chimerakang/simple_file_server/downloads chimerakang/simeple_file_server
